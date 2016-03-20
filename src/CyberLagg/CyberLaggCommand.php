b9438e6e22c483be9f67f924074145e1d16a7002
Varfalomey <qaoble@inbox.ru>
2016-03-20 16:45:53 +0400

    Update CyberLaggCommand.php
    Add reload plugin

--- src/CyberLagg/CyberLaggCommand.php
+++ src/CyberLagg/CyberLaggCommand.php
@@ -8,58 +8,59 @@ use pocketmine\command\PluginIdentifiableCommand;
 
 class CyberLaggCommand extends Command implements PluginIdentifiableCommand {
 
-  public $plugin;
+    public $plugin;
 
-  public function __construct(Loader $plugin) {
-    parent::__construct("cyberlagg", "§aMenghapus lagg!", "/cyberlagg <check/clear/killmobs/clearall>", ["lagg"]);
-    $this->setPermission("cyberlagg.command.clearlagg");
-    $this->plugin = $plugin;
-  }
-
-  public function getPlugin() {
-    return $this->plugin;
-  }
-
-  public function execute(CommandSender $sender, $alias, array $args) {
-    if(!$this->testPermission($sender)) {
-      return false;
+    public function __construct(Loader $plugin) {
+        parent::__construct("cyberlagg", "§aMenghapus lagg!", "/cyberlagg <check/clear/killmobs/clearall>", ["lagg"]);
+        $this->setPermission("cyberlagg.command.clearlagg");
+        $this->plugin = $plugin;
     }
-    if(isset($args[0])) {
-      switch($args[0]) {
-        case "clear":
-         $sender->getServer() ->broadcastMessage("§4[§eCyber§bLagg§4] Menghapus " . $this->getPlugin()->removeEntities() . " §7entities.");
-          return true;
-        case "check":
-        case "count":
-          $c = $this->getPlugin()->getEntityCount();
-          $sender->getServer() ->broadcastMessage("§4[§eCyber§bLagg§4] §bTerdeteksi " . $c[0] . " pemain, " . $c[1] . " mobs, dan " . $c[2] . " entities.");
-          return true;
-        case "reload":
-          // TODO
-          return true;
-        case "killmobs":
-          $sender->getServer() ->broadcastMessage("§4[§eCyber§bLagg§4] Menghapus " . $this->getPlugin()->removeMobs() . " §7mobs");
-          return true;
-        case "clearall":
-         $sender->getServer() ->broadcastMessage("§4[§eCyber§bLagg] Menghapus " . ($d = $this->getPlugin()->removeMobs()) . " §7mobs" . ($d == 1 ? "" : "s") . " dan " . ($d = $this->getPlugin()->removeEntities()) . " entit" . ($d == 1 ? "y" : "ies") . ".");
-          return true;
-        case "area":
-          // TODO
-          return true;
-        case "unloadchunks":
-          // TODO
-          return true;
-        case "chunk":
-          // TODO
-          return true;
-        case "tpchunk":
-          // TODO
-          return true;
-        default:
-          return false;
-      }
+
+    public function getPlugin() {
+        return $this->plugin;
     }
-    return false;
-  }
+
+    public function execute(CommandSender $sender, $alias, array $args) {
+        if(!$this->testPermission($sender)) {
+            return false;
+        }
+        if(isset($args[0])) {
+            switch($args[0]) {
+                case "clear":
+                    $sender->getServer() ->broadcastMessage("§4[§eCyber§bLagg§4] Menghapus " . $this->getPlugin()->removeEntities() . " §7entities.");
+                    return true;
+                case "check":
+                case "count":
+                    $c = $this->getPlugin()->getEntityCount();
+                    $sender->getServer() ->broadcastMessage("§4[§eCyber§bLagg§4] §bTerdeteksi " . $c[0] . " pemain, " . $c[1] . " mobs, dan " . $c[2] . " entities.");
+                    return true;
+                case "reload":
+                    $sender->getServer()->getPluginManager()->disablePlugin($this);
+                    $sender->getServer()->getPluginManager()->enablePlugin($this);
+                    return true;
+                case "killmobs":
+                    $sender->getServer() ->broadcastMessage("§4[§eCyber§bLagg§4] Menghapus " . $this->getPlugin()->removeMobs() . " §7mobs");
+                    return true;
+                case "clearall":
+                    $sender->getServer() ->broadcastMessage("§4[§eCyber§bLagg] Menghapus " . ($d = $this->getPlugin()->removeMobs()) . " §7mobs" . ($d == 1 ? "" : "s") . " dan " . ($d = $this->getPlugin()->removeEntities()) . " entit" . ($d == 1 ? "y" : "ies") . ".");
+                    return true;
+                case "area":
+                    // TODO
+                    return true;
+                case "unloadchunks":
+                    // TODO
+                    return true;
+                case "chunk":
+                    // TODO
+                    return true;
+                case "tpchunk":
+                    // TODO
+                    return true;
+                default:
+                    return false;
+            }
+        }
+        return false;
+    }
 
 }
